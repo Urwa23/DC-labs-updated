c57b3313270b64ba6f63bc03c3252ab6cbb736b6
Urwa23 <urwahmaqsood23@gmail.com>
2021-12-01 19:29:18 +0500

    updated

---
+++ Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.Threading.Tasks;
+
+
+
+namespace lab2
+{
+    class DataFunctions
+    {
+        public DataTable getData(string SQL)
+        {
+            DataTable DT = new DataTable();
+            string conStr = "Data Source=.;Initial Catalog=dc;Integrated Security=True";
+            SqlConnection ConObj = new SqlConnection(conStr);
+            ConObj.Open();
+            SqlDataAdapter da = new SqlDataAdapter(SQL,ConObj);
+            da.Fill(DT);
+            ConObj.Close();
+            return DT;
+            
+            
+
+        }
+        public bool InsertData(string SQL_Insert)
+        {
+            string conStr = "Data Source=.;Initial Catalog=dc;Integrated Security=True";
+            SqlConnection ConObj = new SqlConnection(conStr);
+            ConObj.Open();
+            SqlCommand cmd = new SqlCommand(SQL_Insert, ConObj);
+            int rowsAffected = 0;
+            rowsAffected = cmd.ExecuteNonQuery();
+            if (rowsAffected > 0)
+            {
+                return true;
+
+            }
+            else return false;
+        }
+        
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataFunctions dataObj = new lab2.DataFunctions();
+            string SQL_Slect = "Select * from city";
+            dataGridView1.DataSource = dataObj.getData(SQL_Slect);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataFunctions dataObj = new lab2.DataFunctions();
+            string SQL_Join = "Select * from uni inner join city on uni.cityId=city.cityId";
+            dataGridView1.DataSource = dataObj.getData(SQL_Join);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataFunctions dataObj = new lab2.DataFunctions();
+            string SQL_Join = "Select * from Course inner join sr on Course.courseid=sr.courseid";
+            dataGridView1.DataSource = dataObj.getData(SQL_Join);
+        }
+    }
+}
